7e47789e64764ff255ad3a5749c9285c004dd595
merv-e <34655454+merv-e@users.noreply.github.com>
2024-05-04 17:22:35 +0300

    two new routes are added.

--- src/app/App.js
+++ src/app/App.js
@@ -28,6 +28,8 @@ const router = createBrowserRouter(
       <Route path="articles" element={<Articles/>}/>
       <Route path="categories" element={<Categories/>}/>
       <Route path="profile" element={<Profile/>}/>
+      <Route path="articles/:title" element={<Article/>}/>
+      <Route path="authors/:name" element={<Author/>}/>
     </Route>
   )
 );
