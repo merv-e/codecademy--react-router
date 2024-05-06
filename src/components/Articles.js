58f0b38ed7c03530b88ee38e4fdc8410251cae1c
merv-e <34655454+merv-e@users.noreply.github.com>
2024-05-06 23:19:20 +0300

    useSearchPArams hook is used  for search parameters

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -2,17 +2,14 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
 import Search from "./Search";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
-// Import Link and useSearchParams from React Router
 
 export default function Articles () {
   const articles = useSelector(selectArticles);
 
-  // Grab URLSearchParams object from useSearchParams hook
-  
-  // Get the queryParams from object returned from useSearchParams and set to `title`
-  const title = '';
+  const [searchParams, setSearchParams] = useSearchParams();
+  const title = searchParams.get("title");
 
   const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
 
